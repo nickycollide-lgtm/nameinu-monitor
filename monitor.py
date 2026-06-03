a06feef50e4e27979445b24ed53f17cca36a323c
nickycollide-lgtm <nickycollide@gmail.com>
2026-06-03 15:11:15 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,12 +1,5 @@
-import requests
+import cloudscraper
 
-URL = "https://nameinu.info/list?sex=woman&age=all&region=kanto"
+scraper = cloudscraper.create_scraper()
 
-html = requests.get(
-    URL,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    }
-).text
-
-print(html[:3000])
+html = scraper.get(url).text
